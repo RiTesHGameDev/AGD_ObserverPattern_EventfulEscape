3943f58f626065598503ead26156f3303c4b0e5f
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-09 20:47:35 +0530

    Event Architecture

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -5,5 +5,5 @@ public class EventController
     public Action baseEvent;
     public void AddListener(Action listener) => baseEvent += listener;
     public void RemoveListener(Action listener)=>baseEvent -= listener;
-    public void Invoke() => baseEvent?.Invoke();
+    public void InvokeEvent() => baseEvent?.Invoke();
 }
