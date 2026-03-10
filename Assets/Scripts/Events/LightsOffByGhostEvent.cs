426bb5df792a0e3ed752101e153c5e1cb366277f
Ritesh Kumar Behera <ritz.rb50@gmail.com>
2026-03-10 17:03:35 +0530

    key required for ghost event

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int keysReuiredToTrigger;
+    [SerializeField] private SoundType soundType;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerView>() != null && keysReuiredToTrigger == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+            this.enabled = false;
+        }
+    }
 }
